4fb04a94be1d97909be70f00434ab2a9b6796fff
Kris Lui <68591724+krissolui@users.noreply.github.com>
2023-10-14 13:41:23 -0400

    updated router layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import App from './App.tsx';
+import './index.css';
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false,
+			refetchOnMount: false,
+			refetchOnReconnect: false,
+			retry: false,
+		},
+	},
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<QueryClientProvider client={queryClient}>
+			<App />
+		</QueryClientProvider>
+	</React.StrictMode>
+);
--- src/router.tsx
+++ src/router.tsx
@@ -1,27 +1,34 @@
 import { createBrowserRouter } from 'react-router-dom';
 import './App.css';
-import Home from './pages/Home';
-import Favorites from './pages/Favorites';
-import NotFound from './pages/NotFound';
-import CharityDetail from './pages/CharityDetail';
-import Search from './pages/Search';
+import HomePage from './pages/HomePage';
+import FavoritesPage from './pages/FavoritesPage';
+import NotFoundPage from './pages/NotFoundPage';
+import SearchPage from './pages/SearchPage';
+import CharityDetailPage from './pages/CharityDetailPage';
+import RootPage from './pages/RootPage';
 
 export const router = createBrowserRouter([
 	{
 		path: '/',
-		errorElement: <NotFound />,
-		element: <Home />,
-	},
-	{
-		path: '/charity/:id',
-		element: <CharityDetail />,
-	},
-	{
-		path: '/search/:search',
-		element: <Search />,
-	},
-	{
-		path: '/favorites',
-		element: <Favorites />,
+		element: <RootPage />,
+		errorElement: <NotFoundPage />,
+		children: [
+			{
+				path: '/',
+				element: <HomePage />,
+			},
+			{
+				path: '/charity/:id',
+				element: <CharityDetailPage />,
+			},
+			{
+				path: '/favorites',
+				element: <FavoritesPage />,
+			},
+			{
+				path: '/search/:cause',
+				element: <SearchPage />,
+			},
+		],
 	},
 ]);
