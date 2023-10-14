765b785b4f15505f6f7a3c88728f808b206e9879
Kris Lui <68591724+krissolui@users.noreply.github.com>
2023-10-14 13:40:44 -0400

    integrated every.org

---
+++ src/api/everyOrg.ts
@@ -0,0 +1,40 @@
+import axios from 'axios';
+import { EVERY_API_KEY, EVERY_ENDPOINT } from '../utils/constants';
+import { Charity, CharityDetail, Tag } from '../utils/types';
+
+interface NonprofitsResponse {
+	nonprofits: Charity[];
+}
+
+interface NonprofitResponse {
+	data: {
+		nonprofit: CharityDetail;
+		nonprofitTags: Tag[];
+	};
+}
+
+export const searchNonprofits = async (searchTerm: string) => {
+	try {
+		const res = await axios<NonprofitsResponse>({
+			method: 'get',
+			baseURL: EVERY_ENDPOINT,
+			url: `/search/${searchTerm}?apiKey=${EVERY_API_KEY}`,
+		});
+
+		return res.data;
+	} catch (ex) {
+		return {
+			nonprofits: [],
+		};
+	}
+};
+
+export const getNonprofit = async (identifier: string) => {
+	const res = await axios<NonprofitResponse>({
+		method: 'get',
+		baseURL: EVERY_ENDPOINT,
+		url: `/nonprofit/${identifier}?apiKey=${EVERY_API_KEY}`,
+	});
+
+	return res.data;
+};
